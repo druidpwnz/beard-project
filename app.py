03e51bf7a80e0c6443055aedc8a0dc82c55e7d60
Igor Nest <druidd95@gmail.com>
2023-07-20 01:15:55 +0300

    base.html and routes has been added

--- app.py
+++ app.py
@@ -8,5 +8,25 @@ def index():
     return render_template("beard.html")
 
 
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+@app.route("/appointment")
+def appointment():
+    return render_template("appointment.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
