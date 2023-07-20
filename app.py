3c499825eff40c59a1c62b48577b5e7d2f7b2180
Igor Nest <druidd95@gmail.com>
2023-07-20 15:05:22 +0300

    appointment form has been added

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, TelField, EmailField, DateTimeLocalField
+from wtforms.validators import DataRequired, Length
+import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
+
+
+class AppointmentForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), Length(max=30)])
+    email = EmailField("Email", validators=[DataRequired()])
+    phone = TelField("Phone", validators=[DataRequired()])
+    convenient_time = DateTimeLocalField(
+        "Convenient Time", validators=[DataRequired()], format="%Y-%m-%dT%H:%M"
+    )
 
 
 @app.route("/")
@@ -23,9 +37,19 @@ def gallery():
     return render_template("gallery.html")
 
 
-@app.route("/appointment")
+@app.route("/appointment", methods=["GET", "POST"])
 def appointment():
-    return render_template("appointment.html")
+    form = AppointmentForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        phone = form.phone.data
+        convenient_time = form.convenient_time.data
+
+        return f"<h1>Name {name}, Email {email}, Phone {phone}, Convenient time {convenient_time}</h1>"
+
+    return render_template("appointment.html", form=form)
 
 
 if __name__ == "__main__":
